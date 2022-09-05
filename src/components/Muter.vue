68940e223793fc82f10487ea06e43c9bc4227896
Stas <atlantisscale@gmail.com>
2022-09-05 20:29:57 +0300

    ref select

--- src/components/Muter.vue
+++ src/components/Muter.vue
@@ -1,6 +1,8 @@
 <template>
 <doter :foo='faza'></doter>
-	<input type="text" :value='got'>
+	<input type="text" ref="gott">
+
+  <button @click='fox' class="opo" :class='{spg:foxy}'>Ok</button>
 
 </template>
 
@@ -22,11 +24,26 @@ components:{
       {valuess:'chankala',name:'Хвощ'},
       {valuess:'zizan',name:'Пудра'}
 
-      ]
+      ],
+      foxy:false
+    }
+  },
+  methods:{
+    fox(){
+     console.log(this.$refs.gott.value=555) 
+      this.foxy=true
     }
   }
 }
 </script>
 
 <style lang="css" scoped>
+  .opo{
+    color: red;
+  }
+
+.spg{
+  color: gold;
+}
+
 </style>
